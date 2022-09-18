ddbc07bef90502c3f60ebda97298e210c0b6163b
Ramon Williams <ramnwor21@gmail.com>
2022-09-18 19:45:56 +0200

    Experience css

--- app/src/components/ExperienceCard/ExperienceCard.jsx
+++ app/src/components/ExperienceCard/ExperienceCard.jsx
@@ -7,33 +7,13 @@ const ExperienceCard = ({ experience }) => {
 
   return (
     <Link to={`/experience/${experience._id}`}>
-    <figure className="a-box">
-      <div className="img-container"> 
-
-        <div className="img-inner">
-
-            <div className="inner-skew">
-            <img className="card_image" src={experience.image} alt={experience.name} />
-            <h4 className="name">{experience.name}</h4>  
-            <p>{experience.puntuation}</p>   
-        
-   
-            </div>
-
-        </div>
-      
-      </div>
-        
+    <figure className="carta">            
+        <div className="image-experience">
+            <img src={experience.image} alt={experience.name} />                                    
+        </div>            
         <div className="text-container">
-
-          <h3>{experience.name}</h3> 
-          <p>Precio:{experience.price}</p>
-        </div>
-         <div>
-           
-         </div>
-        
-        
+          <h3>{experience.name}</h3>         
+        </div>          
     </figure>
 </Link>
   );
